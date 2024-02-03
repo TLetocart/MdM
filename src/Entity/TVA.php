5fb7fa72fc3f0ab0f3e91f366fad3d751a66ba3d
Letocart Thomas <tom.96@live.fr>
2024-02-03 18:45:23 +0100

    Doctrine TVA

---
+++ src/Entity/TVA.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TVARepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TVARepository::class)]
+class TVA
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $libelle = null;
+
+    #[ORM\Column]
+    private ?float $valeur = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): static
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getValeur(): ?float
+    {
+        return $this->valeur;
+    }
+
+    public function setValeur(float $valeur): static
+    {
+        $this->valeur = $valeur;
+
+        return $this;
+    }
+}
